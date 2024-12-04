19de9af1273c9f7eb80a1952255a93fd341a7652
Ethan <175902687+kokorocks@users.noreply.github.com>
2024-12-04 15:08:38 -0500

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,1 +1,17 @@
 #python for now
+'''{
+  "custom": "print(data)",
+  "og": "console.log(data)",
+  "param": "data"
+},
+{
+  "custom": "error(errortxt)",
+  "og": "console.error(errortxt)",
+  "param": "errortxt"
+}
+{
+  "custom": "def <name> (input){}",
+  "og": "function <name> (input){}",
+  "param": "input",
+  "func-name":"name",
+}'''
